113febfb1b716b470bd6f2d93f327146bf3a6f57
Moritz Winter <me@mo42.de>
2024-05-22 00:05:37 +0200

    Read SQL from file

--- src/main.rs
+++ src/main.rs
@@ -8,15 +8,34 @@ use crate::visitor::{SqlVisitor, traverse_ast};
 mod generator;
 use crate::generator::generate_code;
 
+use std::env;
+use std::fs;
+use std::process;
+
 
 fn main() {
-    let sql = "SELECT cc FROM 'ta.csv' WHERE cb = 1 AND ca = 2";
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        eprintln!("Usage: {} <file_path>", args[0]);
+        process::exit(1);
+    } else {
+   match fs::read_to_string(&args[1]) {
+        Ok(read_file) => {
+
     let dialect = GenericDialect {};
-    let ast = Parser::parse_sql(&dialect, sql).unwrap();
-    dbg!("{:?}", &ast);
+    let ast = Parser::parse_sql(&dialect, read_file).unwrap();
+
     let mut visitor = SqlVisitor::new();
     for statement in ast {
         traverse_ast(&mut visitor, &statement);
     }
     generate_code(&visitor.from.unwrap(), &visitor.selection, &visitor.filter);
+        }
+        Err(error) => {
+            eprintln!("Error reading file {}: {}", &args[1], error);
+            process::exit(1);
+        }
+    }
+    }
+
 }
