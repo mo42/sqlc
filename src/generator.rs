1838d757925c086d8425b010385ec2cba764d27b
Moritz Winter <me@mo42.de>
2024-06-07 21:14:15 +0200

    Rustfmt

--- src/generator.rs
+++ src/generator.rs
@@ -1,7 +1,7 @@
 use csv::Reader;
+use std::collections::{HashMap, HashSet};
 use std::error::Error;
 use std::fs::File;
-use std::collections::{HashMap, HashSet};
 
 pub struct Schema {
     pub index_type: String,
@@ -16,7 +16,12 @@ fn get_column_names(filename: &String) -> Result<Vec<String>, Box<dyn Error>> {
     Ok(column_names)
 }
 
-pub fn generate_code(from: &String, selection: &Vec<String>, filter: &Vec<String>, filter_cols: &HashSet<String>) {
+pub fn generate_code(
+    from: &String,
+    selection: &Vec<String>,
+    filter: &Vec<String>,
+    filter_cols: &HashSet<String>,
+) {
     println!("#include <DataFrame/DataFrame.h>\n\n#include <iostream>");
     println!("using namespace hmdf;");
     // TODO analyse source file here to determine all types
@@ -28,46 +33,59 @@ pub fn generate_code(from: &String, selection: &Vec<String>, filter: &Vec<String
         index_type: "std::string".to_string(),
         col_types: col_types,
     };
-    println!("typedef {idx_t} idx_t;", idx_t=schema.index_type);
+    println!("typedef {idx_t} idx_t;", idx_t = schema.index_type);
     println!("using SqlcDataFrame = StdDataFrame<idx_t>;");
     println!("int main(int, char**) {{");
     println!("  SqlcDataFrame df;");
-    println!("  df.read(\"{file_name}\", io_format::csv2);", file_name=from);
+    println!(
+        "  df.read(\"{file_name}\", io_format::csv2);",
+        file_name = from
+    );
     print!("  auto where_functor = [](const idx_t&");
     for col in filter_cols.iter() {
-        print!(", const {col_t} &{col}", col_t=schema.col_types.get(col).unwrap(), col=col);
+        print!(
+            ", const {col_t} &{col}",
+            col_t = schema.col_types.get(col).unwrap(),
+            col = col
+        );
     }
     println!(") -> bool {{");
     print!("    return ");
     for filter_token in filter.iter() {
-        print!("{filter_token}", filter_token=filter_token);
+        print!("{filter_token}", filter_token = filter_token);
     }
     println!(";");
     println!("  }};");
     println!("  auto where_df =");
     print!("    df.get_data_by_sel<");
     for col in filter_cols.iter() {
-        print!("{col_t}, ", col_t=schema.col_types.get(col).unwrap());
+        print!("{col_t}, ", col_t = schema.col_types.get(col).unwrap());
     }
     print!("decltype(where_functor)");
     let distinct_col_types: HashSet<_> = schema.col_types.values().cloned().collect();
     for col_type in distinct_col_types {
-        print!(", {col_t}", col_t=col_type);
+        print!(", {col_t}", col_t = col_type);
     }
     print!(">(");
     for col in filter_cols.iter() {
-        print!("\"{col}\", ", col=col);
+        print!("\"{col}\", ", col = col);
     }
     println!("where_functor);");
     println!("  std::vector<idx_t> idx = where_df.get_index();");
     for col in selection.iter() {
-        println!("  std::vector<{col_t}> {col} = where_df.get_column<{col_t}> (\"{col}\");",
-                col_t=schema.col_types.get(col).unwrap(), col=col);
+        println!(
+            "  std::vector<{col_t}> {col} = where_df.get_column<{col_t}> (\"{col}\");",
+            col_t = schema.col_types.get(col).unwrap(),
+            col = col
+        );
     }
     println!("  SqlcDataFrame select;");
     println!("  select.load_index(std::move(idx));");
     for col in selection.iter() {
-        println!("  select.load_column(\"{col}\", std::move({col}));", col=col);
+        println!(
+            "  select.load_column(\"{col}\", std::move({col}));",
+            col = col
+        );
     }
     println!("  std::cout << select.to_string<double>() << std::endl;");
     println!("  return 0;");
