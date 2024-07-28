9c21947e40583a299cb2907497126f76084c72d5
Moritz Winter <me@mo42.de>
2024-07-28 18:56:46 +0200

    Extract join info from AST

--- example.sql
+++ example.sql
@@ -1,1 +1,8 @@
-SELECT date, column2 FROM 'example.csv' WHERE column3 = 1089200 OR column2 = 3
+SELECT
+  date,
+  column2
+FROM
+  'example.csv'
+  JOIN 'join.csv' USING(column2)
+WHERE
+  column3 = 1089200 OR column2 = 3
