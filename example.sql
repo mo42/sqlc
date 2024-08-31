89d7030272f080e51e25cac4b5155ff7d72d2350
Moritz Winter <me@mo42.de>
2024-08-31 21:59:11 +0200

    Simple aliasing feature and correct order
    - Simple aliasing: col1 AS alias,
    - Fix SQL execution order and add documentation

--- example.sql
+++ example.sql
@@ -1,9 +1,9 @@
 SELECT
-  date,
+  date AS date_alias,
   column2
 FROM
   'example.csv'
   JOIN 'join.csv' USING(column2)
 WHERE
   joined_string = "Join string 3"
-ORDER BY date ASC, column2 DESC
+ORDER BY date_alias ASC, column2 DESC
