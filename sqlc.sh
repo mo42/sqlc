a419ac8953628d36d583b7522015f3902da3e717
Moritz Winter <me@mo42.de>
2024-12-23 22:14:53 +0100

    Extended compile script

--- sqlc.sh
+++ sqlc.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
 
+if [ -z "$1" ]; then
+  echo "No SQL file provided for compilation."
+  exit 1
+fi
+if [ ! -f "$1" ]; then
+  echo "The file '$1' does not exist."
+  exist 1
+fi
+
 cargo run -- $1 > $(basename -s sql $1)cpp
 clang-format -i $(basename -s sql $1)cpp
+clang -lstdc++ -lm -std=c++23 -o $(basename -s .sql $1) -Idf/include $(basename -s sql $1)cpp df/lib64/libDataFrame.a
